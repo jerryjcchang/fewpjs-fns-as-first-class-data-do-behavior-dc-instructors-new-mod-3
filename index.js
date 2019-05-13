02967db9cb67eb4b18cdd525418ea462c505c31c
JC Chang <jerry.chang@flatironschool.com>
2019-05-13 13:31:34 -0400

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,25 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+const form = document.getElementById('')
+
+function greet(timeString){
+  debugger
+  const splitTime = timeString.split(":")
+  let hour = parseInt(splitTime[0])
+
+  if(hour < 12){
+    return "Good Morning"
+  } else if(hour >= 12 && hour < 17){
+    return "Good Afternoon"
+  } else if(hour >= 17){
+    return "Good Evening"
+  }
+}
+
+function displayMessage(message){
+  document.getElementById('greeting').textContent = message
+}
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
